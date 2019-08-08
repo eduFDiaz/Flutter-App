62a4a1f4a9c5ca7e1815935698ae16f791945a65
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-08-07 22:32:41 -0500

    base structure for any given Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 Future main(List<String> args) async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
             appBar: AppBar(
               title: Text("EasyList"),
@@ -17,12 +26,12 @@ class MyApp extends StatelessWidget {
                 Container(
                   margin: EdgeInsets.all(5.0),
                   child: RaisedButton(
-                  onPressed: () {
-                    print("clicked!\n");
-                  },
-                  child: Text("Add Product"),
-                ),),
-                
+                    onPressed: () {
+                      print("clicked!\n");
+                    },
+                    child: Text("Add Product"),
+                  ),
+                ),
                 Card(
                   child: Column(
                     children: <Widget>[
